478153bf91def89cc82eb6de64f407f466cfd819
talioh <T123456t>
2019-03-12 09:59:45 +0200

    expand json + CR changes

--- src/main/java/com/nice/mcr/injector/service/Consts.java
+++ src/main/java/com/nice/mcr/injector/service/Consts.java
@@ -16,11 +16,11 @@ public class Consts {
     static final String OTHER_SWITCH_ID = "iOtherSwitchID";
     static final String INTERACTION_QA_TYPE_ID = "bitInteractionQATypeID";
     static final String INTERACTION_TYPE_ID = "tiInteractionTypeID";
-    static final String INTERACTION_RECORDD_TYPE_ID = "tiInteractionRecordedTypeID";
+    static final String INTERACTION_RECORDED_TYPE_ID = "tiInteractionRecordedTypeID";
     static final String IS_EVALUATED = "bitIsEvaluated";
     static final String IS_CUSTOMER_EVALUATED = "bitIsCustomerEvaluated";
     static final String MEDIA_TYPES_ID = "iMediaTypesId";
-    static final String INTERACTION_DESC = "vcInteractionDesc";
+    static final String INTERACTION_TYPE_DESC = "vcInteractionDesc";
     static final String INITIATOR_TYPE_ID = "siInitiatorTypeID";
     static final String INITIATOR_TYPE_DESC = "vcInitiatorTypeDesc";
     static final String EMAIL_REF_ID = "vcEmailRefID";
@@ -54,19 +54,16 @@ public class Consts {
     static final String TRUNK_GROUP = "vcTrunkGroup";
     static final String TRUNK_NUMBER = "vcTrunkNumber";
     static final String TRUNK_LABEL = "nvcTrunkLabel";
-    static final String DAILED_NUMBER = "nvcDialedNumber";
+    static final String DIALED_NUMBER = "nvcDialedNumber";
     static final String CLIENT_ID = "iClientID";
     static final String VIRTUAL_DEVICE_ID = "iVirtualDeviceID";
     static final String PARTICIPANT_TYPE_ID = "tiParticipantTypeID";
     static final String PARTICIPANT_TYPE_DESC = "vcParticipantTypeDesc";
-    static final String OPEN_REASON_TYPE = "iOpenReasonTypeID";
     static final String OPEN_REASON_DESC = "vcOpenReasonDesc";
-    static final String CLOSE_REASON_TYPE = "iCloseReasonTypeID";
     static final String CLOSE_REASON_DESC = "vcCloseReasonDesc";
     static final String DEVICE_TYPE_DESC = "vcDeviceTypeDesc";
     static final String RECORDING_SIDE_TYPE_ID = "tiRecordingSideTypeID";
     static final String RECORDING_SIDE_DESC = "vcRecordingSideDesc";
-    static final String MEDIA_TYPE_ID = "iMediaTypeId";
     static final String MEDIA_DESC = "vcMediaDesc";
     static final String DIRECTION_TYPE_ID = "tiDirectionTypeID";
     static final String DIRECTION_TYPE_DESC = "vcDirectionTypeDesc";
@@ -78,7 +75,7 @@ public class Consts {
     static final String RECORDING_GMT_STOP_TIME = "dtRecordingGMTStopTime";
     static final String RECORDING_RECORDED_TYPE_ID = "tiRecordingRecordedTypeID";
     static final String PROGRAM_ID = "iProgramID";
-    static final String RECORDING_PARTICIPANT_ID = "iRecordedParticipantID";
+    static final String RECORDED_PARTICIPANT_ID = "iRecordedParticipantID";
     static final String TIME_DIFF = "biTimeDiff";
     static final String WRAPUP_TIME = "biWrapupTime";
     static final String SESSION_ID = "biSessionID";
@@ -91,9 +88,55 @@ public class Consts {
     static final String CONTACT_ID = "iContactID";
     static final String ITEM_SEQUENCE_NUMBER = "iItemSequenceNumber";
     static final String TIME_STAMP = "dtTimeStamp";
-    static final String ITEM_UDER_ID = "iItemUserID";
+    static final String ITEM_USER_ID = "iItemUserID";
     static final String ITEM_VALUE = "nvcItemValue";
-    static final String IS_DELETED = "bitIsDeleted";
+    static final String ITEM_IS_DELETED = "bitIsDeleted";
+
+    static final String ITEM_IS_PUBLIC = "bitIsPublic";
+    static final String ITEM_OFFSET = "iOffset";
+    static final String RECORDED_TYPE_ID = "tiRecordedTypeID";
+    static final String RECORDED_TYPE_DESC = "vcRecordedTypeDesc";
+    static final String ARCHIVE_ID = "iArchiveID";
+    static final String MEDIA_TYPE_ID = "tiMediaTypeId";
+    static final String ARCHIVE_PATH = "vcArchivePath";
+    static final String ARCHIVE_ID_HIGH = "iArchiveIdHigh";
+    static final String ARCHIVE_ID_LOW = "iArchiveIdLow";
+    static final String ARCHIVE_CLASS = "iArchiveClass";
+    static final String SC_SERVER_ID = "vcScServerId";
+    static final String SC_SITE_ID = "iSiteID";
+    static final String SC_RULE_ID = "iRuleID";
+    static final String SC_LOGGER_ID = "iLoggerID";
+    static final String SC_LOGGER_RESOURCE = "iLoggerResource";
+    static final String ARCHIVE_UNIQUE_ID = "vcArchiveUniqueId";
+    static final String RETENTION_DAYS = "iRetentionDays";
+    static final String CONTACT_GMT_START_TIME = "dtContactGMTStartTime";
+    static final String CONTACT_GMT_STOP_TIME = "dtContactGMTStopTime";
+    static final String CONTACT_DURATION = "biContactDuration";
+    static final String CONTACT_OPEN_REASON_ID = "iContactOpenReasonID";
+    static final String CONTACT_CLOSE_REASON_ID = "iContactCloseReasonID";
+    static final String TRANSFER_SITE_ID = "iTransferSiteID";
+    static final String TRANSFER_CONTACT_ID = "iTransferContactID";
+    static final String CONTACT_RECORDED_TYPE_ID = "tiContactRecordedTypeID";
+    static final String CONTACT_QA_TYPE_ID = "bitContactQATypeID";
+    static final String CONTACT_DIRECTION_TYPE_ID = "tiContactDirectionTypeID";
+    static final String EXCEPTION_TYPE_ID = "iExceptionTypeID";
+    static final String EXCEPTION_POSSIBLE_CAUSE = "nvcPossibleCause";
+    static final String EXCEPTION_RECOMMENDED_ACTION = "nvcRecomendedAction";
+    static final String EXCEPTION_NUMBER = "iExceptionNumber";
+    static final String EXCEPTION_TIMESTAMP = "dtExceptionTimeStamp";
+    static final String EXCEPTION_DETAIL = "vcExceptionDetail";
+    static final String CREATED_BY_ID = "iCreatedByID";
+    static final String CLIP_NAME = "nvcClipName";
+    static final String CATEGORY_ID = "iCategoryID";
+    static final String IS_PUBLISHED = "bIsPublished";
+    static final String IS_PUBLIC = "bIsPublic";
+    static final String CLIP_DURATION = "biClipDuration";
+    static final String IS_DELETED = "bIsDeleted";
+    static final String I_INTERACTION_TYPE_ID = "iInteractionTypeID";
+    static final String TASK_ID = "iTaskID";
+    static final String FLAG_ID = "iFlagID";
+    static final String USER_SITE_ID = "iUserSiteID";
+    static final String MODIFICATION_TIME = "dtModificationTime";
 
 }
 
@@ -135,6 +178,7 @@ enum OpenCallReason{
 
 }
 
+
 enum CloseCallReason{
     Unknown(0),
     Normal_Start(1),
@@ -167,6 +211,7 @@ enum CloseCallReason{
     }
 }
 
+
 enum InteractionType{
     Unknown(0),
     Clip_Recording(1),
@@ -196,3 +241,262 @@ enum InteractionType{
     }
 }
 
+
+enum MediaTypes{
+    Unknown(0),
+    Screen(1),
+    Voice(2),
+    Email(4),
+    Chat(8),
+    Web(16),
+    Video(32),
+    SMS(64),
+    Verbatim(100),
+    OriginalEmail(128),
+    Survey(256),
+    Survey_WebComplaint(512),
+    Survey_Verbatim(1024);
+
+    private final int mediaTypeID;
+    private MediaTypes(int mediaTypeID){
+        this.mediaTypeID = mediaTypeID;
+    }
+
+    public int getMediaTypeID(){
+        return mediaTypeID;
+    }
+
+    public static MediaTypes getRandomMediaType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+
+enum InitiatorType{
+    No_Value(0),
+    Rec_Prog(1),
+    Supervisor(2),
+    Agent(4),
+    Host(8),
+    QA(16),
+    Total(32),
+    ROD(64),
+    Monitor(256),
+    SEA(512),
+    Clip_Recorder(1024),
+    TRS(2048),
+    Internal_Nice_Application(4096),
+    Hold(8192);
+
+    private final int InitiatorTypeID;
+    private InitiatorType(int InitiatorTypeID){
+        this.InitiatorTypeID = InitiatorTypeID;
+    }
+
+    public int getInitiatorTypeID(){
+        return InitiatorTypeID;
+    }
+
+    public static InitiatorType getRandomInitiatorType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+
+enum DirectionType{
+    Unknown(0),
+    Incoming(1),
+    Outgoing(2),
+    Internal(3),
+    Block(4),
+    Tandem(5),
+    External(6);
+
+    private final int directionTypeID;
+    private DirectionType(int directionTypeID){
+        this.directionTypeID = directionTypeID;
+    }
+
+    public int getDirectionTypeID(){
+        return directionTypeID;
+    }
+
+    public static DirectionType getRandomDirectionType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+
+enum ParticipantType{
+    Unknown(0),
+    Internal(1),
+    External(2),
+    Loggers(3);
+
+    private final int participantTypeID;
+    private ParticipantType(int participantTypeID){
+        this.participantTypeID = participantTypeID;
+    }
+
+    public int getparticipantTypeID(){
+        return participantTypeID;
+    }
+
+    public static ParticipantType getRandomParticipantType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+
+enum DeviceType{
+    Unknown(0),
+    Station(1),
+    Line(2),
+    Button(3),
+    Acd(4),
+    Trunk(5),
+    Operator(6),
+    Station_Group(7),
+    Line_Group(8),
+    Button_Group(9),
+    Acd_Group(10),
+    Trunk_Group(11),
+    Operator_Group(12),
+    Vdn(13),
+    Position(14),
+    Agent(15),
+    Ivr(16),
+    Handset(17),
+    Microphone(18),
+    Speaker(19),
+    Rnj(20),
+    Intercom(21),
+    Mobile(22),
+    Other(200);
+
+    private final int DeviceTypeID;
+    private DeviceType(int DeviceTypeID){
+        this.DeviceTypeID = DeviceTypeID;
+    }
+
+    public int DeviceTypeID(){
+        return DeviceTypeID;
+    }
+
+    public static DeviceType getRandomDeviceType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+
+enum RecordingSideType{
+    Unknown(0),
+    In(1),
+    Out(2),
+    Summed(3),
+    Irrelevant(4),
+    Stereo(5);
+
+    private final int RecordingSideTypeID;
+    private RecordingSideType(int RecordingSideTypeID){
+        this.RecordingSideTypeID = RecordingSideTypeID;
+    }
+
+    public int RecordingSideTypeID(){
+        return RecordingSideTypeID;
+    }
+
+    public static RecordingSideType getRandomRecordingSideType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+enum RecordingRecordedType {
+    Unknown(0),
+    Yes(1),
+    No(2),
+    Partial(3);
+
+    private final int recordingRecordedTypeID;
+    private RecordingRecordedType(int recordingRecordedTypeID){
+        this.recordingRecordedTypeID = recordingRecordedTypeID;
+    }
+
+    public int RecorderTypeID(){
+        return recordingRecordedTypeID;
+    }
+
+    public static RecordingRecordedType getRandomRecorderType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+enum ItemDataType {
+    String(1),
+    Number(2),
+    Date(3);
+
+    private final int itemDataTypeID;
+    private ItemDataType(int itemDataTypeID){
+        this.itemDataTypeID = itemDataTypeID;
+    }
+
+    public int itemDataTypeID(){
+        return itemDataTypeID;
+    }
+
+    public static ItemDataType getRandomDataType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+enum CreatorType {
+    Unknown(0),
+    Driver(1),
+    Application(2),
+    User(3);
+
+    private final int creatorTypeID;
+    private CreatorType(int creatorTypeID){
+        this.creatorTypeID = creatorTypeID;
+    }
+
+    public int creatorTypeID(){
+        return creatorTypeID;
+    }
+
+    public static CreatorType getRandomCreatorType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
+
+enum ItemType {
+    Comments(1),
+    Notations(2),
+    Operational_Group_Id(3000),
+    Emergency_Terminal_Address(3001),
+    Party_Address(3002),;
+
+    private final int itemTypeID;
+    private ItemType(int ItemTypeID){
+        this.itemTypeID = ItemTypeID;
+    }
+
+    public int itemTypeID(){
+        return itemTypeID;
+    }
+
+    public static ItemType getRandomItemType() {
+        Random random = new Random();
+        return values()[random.nextInt(values().length)];
+    }
+}
--- src/main/java/com/nice/mcr/injector/service/DataGeneratorImpl.java
+++ src/main/java/com/nice/mcr/injector/service/DataGeneratorImpl.java
@@ -22,8 +22,9 @@ public class DataGeneratorImpl implements DataGenerator {
 
     static Logger logger = (Logger) LoggerFactory.getLogger(DataGeneratorImpl.class);
     Random random = new Random();
-    String stringStartDate = "01-01-2000 00:00:00.00";
-    String stringEndDate = "01-01-2020 00:00:00.00";
+
+    String startData = "01-01-2000 00:00:00.00";
+    String endData = "01-01-2020 00:00:00.00";
     public void createData(int bulkSize, int numOfInteractions) {
         for (int i = 0; i < bulkSize; i++) {
             BufferedWriter writer = null;
@@ -58,36 +59,192 @@ public class DataGeneratorImpl implements DataGenerator {
         ArrayList <String> lastNames = generateNames( numOfInteractions, "..\\tool-elastic-search-injector\\input\\last-names.txt" );
         JSONArray jsonArray = new JSONArray();
         for (int i = 0; i < numOfInteractions; i++) {
+            Date startTime = getRandomDate(this.startData, endData);
+            Date stopTime = getRandomDate(this.startData, endData);
+            OpenCallReason openCallReason = OpenCallReason.getRandomReason();
+            InteractionType interactionType = InteractionType.getRandomInteractionType();
+            CloseCallReason closeCallReason = CloseCallReason.getRandomReason();
+            MediaTypes randomMediaType = MediaTypes.getRandomMediaType();
+            InitiatorType initiatorType = InitiatorType.getRandomInitiatorType();
+            ParticipantType participantType = ParticipantType.getRandomParticipantType();
+            DeviceType deviceType = DeviceType.getRandomDeviceType();
+            RecordingSideType recordingSideType = RecordingSideType.getRandomRecordingSideType();
+            DirectionType randomDirectionType = DirectionType.getRandomDirectionType();
+            ItemDataType randomItemDataType = ItemDataType.getRandomDataType();
+            CreatorType creatorType = CreatorType.getRandomCreatorType();
+            ItemType randomItemType = ItemType.getRandomItemType();
+            RecordingRecordedType recordedType = RecordingRecordedType.getRandomRecorderType();
+
             JSONObject jsonObj = new JSONObject();
             jsonObj.put( Consts.INTERACTION_ID, random.nextInt( 900000 ) + 10000000 );
-
-            Date startTime = getRandomDate(stringStartDate, stringEndDate);
-            Date stopTime = getRandomDate(stringStartDate, stringEndDate);
             jsonObj.put(Consts.INTERACTION_LOCAL_START_TIME, startTime.toString());
             jsonObj.put(Consts.INTERACTION_LOCAL_STOP_TIME, stopTime.toString());
             jsonObj.put(Consts.INTERACTION_GMT_START_TIME, new Date(getGMTTime(startTime)).toString());
             jsonObj.put(Consts.INTERACTION_GMT_STOP_TIME, new Date(getGMTTime(stopTime)).toString());
             jsonObj.put(Consts.INTERACTION_DURATION, String.valueOf(Math.abs(stopTime.getTime() - startTime.getTime())));
-            OpenCallReason openCallReason = OpenCallReason.getRandomReason();
             jsonObj.put(Consts.INTERACTION_OPEN_REASON_ID, openCallReason.getOpenCallReasonID());
-            CloseCallReason closeCallReason = CloseCallReason.getRandomReason();
             jsonObj.put(Consts.INTERACTION_CLOSE_REASON_ID, closeCallReason.getCloseCallReasonID());
             jsonObj.put( Consts.SWITCH_ID, random.nextInt( 9 ) + 1 );
             jsonObj.put( Consts.INITIATOR_USER_ID, random.nextInt( 9 ) + 1 );
             jsonObj.put( Consts.OTHER_SWITCH_ID, random.nextInt( 9 ) + 1 );
-            jsonObj.put( Consts.INTERACTION_QA_TYPE_ID, random.nextInt( 2 ));
-            jsonObj.put( Consts.INTERACTION_QA_TYPE_ID, random.nextInt( 2 ));
-            InteractionType interactionType = InteractionType.getRandomInteractionType();
+            jsonObj.put( Consts.INTERACTION_QA_TYPE_ID, getRandomBit());
+            jsonObj.put( Consts.INTERACTION_QA_TYPE_ID, getRandomBit());
             jsonObj.put( Consts.INTERACTION_TYPE_ID, interactionType.getRandomInteractionTypeID() );
+            jsonObj.put( Consts.INTERACTION_RECORDED_TYPE_ID, interactionType.getRandomInteractionTypeID() );
+            jsonObj.put( Consts.IS_EVALUATED, getRandomBit());
+            jsonObj.put( Consts.IS_CUSTOMER_EVALUATED, getRandomBit());
+            jsonObj.put( Consts.MEDIA_TYPES_ID, randomMediaType.getMediaTypeID());
+            jsonObj.put( Consts.INTERACTION_TYPE_DESC, randomMediaType);
+            jsonObj.put( Consts.INITIATOR_TYPE_ID, initiatorType.getInitiatorTypeID());
+            jsonObj.put( Consts.INITIATOR_TYPE_DESC, initiatorType);
+            jsonObj.put( Consts.CLIENT_DTMF, generateRandomString(5));
+            jsonObj.put( Consts.PBX_CALL_ID, random.nextInt( 2000) + 300000 );
+            jsonObj.put( Consts.EXTERNAL_CALL_ID, random.nextInt(16) );
+            jsonObj.put( Consts.EXTERNAL_CALL_ID, random.nextInt(16) );
+            jsonObj.put( Consts.CALL_DIRECTION_TYPE_ID, randomDirectionType.getDirectionTypeID() );
+            jsonObj.put( Consts.VECTOR_NUMBER, generateRandomString(5));
+            jsonObj.put( Consts.PBX_UNIVARSAL_CALL_INTERACTION_ID, generateRandomString(6));
+            jsonObj.put( Consts.COMPOUND_ID, random.nextInt( 900000 ) + 10000000);
+            jsonObj.put( Consts.NDC_BUSINESS_DATA, generateRandomString(4));
+            jsonObj.put( Consts.BIT_IS_PLAYBACK_CALL, getRandomBit());
+            jsonObj.put( Consts.PARTICIPANT_ID, random.nextInt( 16 ) );
+            jsonObj.put( Consts.STATION, random.nextInt( 900000 ) + 10000000);
+            jsonObj.put( Consts.AGENT_ID, random.nextInt( 900000 ) + 10000000);
+            jsonObj.put( Consts.USER_ID, random.nextInt( 16 ));
+            jsonObj.put( Consts.FIRST_USER, getRandomBit());
+            jsonObj.put( Consts.IS_INERACTION_INITIATOR, getRandomBit());
+            jsonObj.put( Consts.DEVICE_TYPE_ID, deviceType.DeviceTypeID());
+            jsonObj.put( Consts.DEVICE_ID, random.nextInt(256));
+            jsonObj.put( Consts.CTI_AGENT_NAME, firstNames.get( random.nextInt( firstNames.size() ) ));
+            jsonObj.put( Consts.DEPARTMENT, generateRandomString(6));
+            jsonObj.put( Consts.TRUNK_GROUP, generateRandomString(4));
+            jsonObj.put( Consts.TRUNK_NUMBER, generateRandomString(5));
+            jsonObj.put( Consts.TRUNK_LABEL, generateRandomString(5));
+            jsonObj.put( Consts.CLIENT_ID, random.nextInt( 900000 ) + 10000000);
+            jsonObj.put( Consts.VIRTUAL_DEVICE_ID, random.nextInt( 16 ) );
+            jsonObj.put( Consts.PARTICIPANT_TYPE_ID, participantType.getparticipantTypeID() );
+            jsonObj.put( Consts.PARTICIPANT_TYPE_DESC, participantType );
+            jsonObj.put( Consts.OPEN_REASON_DESC, openCallReason );
+            jsonObj.put( Consts.CLOSE_REASON_DESC, closeCallReason );
+            jsonObj.put( Consts.DEVICE_TYPE_DESC, deviceType );
+            jsonObj.put( Consts.RECORDING_SIDE_TYPE_ID, recordingSideType.RecordingSideTypeID() );
+            jsonObj.put( Consts.RECORDING_SIDE_DESC, recordingSideType );
+            jsonObj.put( Consts.MEDIA_DESC, randomMediaType );
+            jsonObj.put( Consts.DIRECTION_TYPE_DESC, randomDirectionType );
+            jsonObj.put( Consts.RECORDING_ID, random.nextInt( 900000 ) + 10000000 );
+            jsonObj.put( Consts.LOGGER, random.nextInt( 100 ) );
+            jsonObj.put( Consts.CHANNEL, random.nextInt( 100 ) - 1 );
+            jsonObj.put( Consts.MML_RECORDING_HINT, generateRandomString(6) );
+            jsonObj.put( Consts.RECORDING_GMT_START_TIME, getGMTTime(startTime) );
+            jsonObj.put( Consts.RECORDING_GMT_STOP_TIME, getGMTTime(stopTime) );
+            jsonObj.put( Consts.RECORDING_GMT_STOP_TIME, getGMTTime(stopTime) );
+            jsonObj.put( Consts.RECORDING_RECORDED_TYPE_ID, RecordingRecordedType.getRandomRecorderType());
+            jsonObj.put( Consts.RECORDING_RECORDED_TYPE_ID, RecordingRecordedType.getRandomRecorderType());
+            jsonObj.put( Consts.PROGRAM_ID, random.nextInt(16) - 1);
+            jsonObj.put( Consts.RECORDED_PARTICIPANT_ID, random.nextInt(16) - 1);
+            jsonObj.put(Consts.TIME_DIFF, String.valueOf(Math.abs(stopTime.getTime() - startTime.getTime())));
+            jsonObj.put(Consts.TIME_DIFF, random.nextInt(16) - 1 );
+            jsonObj.put(Consts.TIME_DIFF, random.nextInt(16) - 1 );
+            jsonObj.put( Consts.SESSION_ID, random.nextInt( 900000 ) + 10000000 );
+            jsonObj.put( Consts.ITEM_DATA_TYPE_ID, randomItemDataType.itemDataTypeID() );
+            jsonObj.put( Consts.ITEM_DATA_TYPE_DESC, randomItemDataType );
+            jsonObj.put( Consts.CREATOR_TYPE_ID, creatorType.creatorTypeID() );
+            jsonObj.put( Consts.CREATOR_TYPE_DESC, creatorType );
+            jsonObj.put( Consts.ITEM_TYPE_ID, randomItemType.itemTypeID() );
+            jsonObj.put( Consts.ITEM_TYPE_ID, randomItemType );
+            jsonObj.put( Consts.CONTACT_ID, random.nextInt(16) - 1 );
+            jsonObj.put( Consts.ITEM_SEQUENCE_NUMBER, random.nextInt(16) - 1 );
+            jsonObj.put( Consts.TIME_STAMP, getRandomDate(startData, endData) );
+            jsonObj.put( Consts.ITEM_USER_ID, random.nextInt(16) - 1 );
+            jsonObj.put( Consts.ITEM_VALUE,  generateRandomString(8) );
+            jsonObj.put( Consts.ITEM_IS_DELETED,  getRandomBit() );
+            jsonObj.put( Consts.IS_PUBLIC,  getRandomBit() );
+            jsonObj.put( Consts.ITEM_OFFSET, random.nextInt(16) - 1 );
+            jsonObj.put( Consts.RECORDED_TYPE_ID,  recordedType.RecorderTypeID());
+            jsonObj.put( Consts.RECORDED_TYPE_DESC,  recordedType);
+            jsonObj.put( Consts.ARCHIVE_ID,  random.nextInt(16) - 1);
+            jsonObj.put( Consts.MEDIA_TYPE_ID, randomMediaType.getMediaTypeID() );
+            jsonObj.put( Consts.MEDIA_TYPE_ID, randomMediaType.getMediaTypeID() );
+            jsonObj.put( Consts.ARCHIVE_PATH, generateRandomString(255));
+            jsonObj.put( Consts.ARCHIVE_ID_HIGH, random.nextInt(16) - 1);
+            jsonObj.put( Consts.ARCHIVE_ID_LOW, random.nextInt(16) - 1);
+            jsonObj.put( Consts.ARCHIVE_CLASS, random.nextInt(16) - 1);
+            jsonObj.put( Consts.SC_SERVER_ID, generateRandomString(100));
+            jsonObj.put( Consts.SC_SITE_ID, random.nextInt(256));
+            jsonObj.put( Consts.SC_RULE_ID, random.nextInt(256));
+            jsonObj.put( Consts.SC_SERVER_ID, random.nextInt(100));
+            jsonObj.put( Consts.SC_LOGGER_ID, random.nextInt(256));
+            jsonObj.put( Consts.SC_LOGGER_RESOURCE, random.nextInt(256));
+            jsonObj.put( Consts.ARCHIVE_UNIQUE_ID, generateRandomString(255));
+            jsonObj.put( Consts.RETENTION_DAYS, random.nextInt(365*30));
+            jsonObj.put( Consts.RETENTION_DAYS, random.nextInt(365*30));
+            jsonObj.put( Consts.CONTACT_GMT_START_TIME, startTime);
+            jsonObj.put( Consts.CONTACT_GMT_STOP_TIME, stopTime);
+            jsonObj.put(Consts.CONTACT_DURATION, String.valueOf(Math.abs(stopTime.getTime() - startTime.getTime())));
+            jsonObj.put(Consts.CONTACT_OPEN_REASON_ID, openCallReason.getOpenCallReasonID());
+            jsonObj.put(Consts.CONTACT_CLOSE_REASON_ID, closeCallReason.getCloseCallReasonID());
+            jsonObj.put( Consts.TRANSFER_SITE_ID, random.nextInt(256));
+            jsonObj.put( Consts.TRANSFER_CONTACT_ID, random.nextInt(256));
+            jsonObj.put( Consts.TRANSFER_SITE_ID, random.nextInt(256));
+            jsonObj.put( Consts.CONTACT_RECORDED_TYPE_ID, random.nextInt(256));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
             jsonObj.put( "tiInteractionRecordedTypeID", 0 );
             jsonObj.put( "vcInteractionDesc", "CTI" );
-            jsonObj.put( "iExternalCallId", -1 );
             jsonObj.put( "tiCallDirectionTypeID", 1 );
-            jsonObj.put( "iPBXCallID", random.nextInt( 32000 - 30000 ) + 300000 );
             jsonObj.put( "iCompoundID", random.nextInt( 900000 ) + 10000000 );
             jsonObj.put( "iParticipantID", random.nextInt( 3 ) + 1 );
             jsonObj.put( "nvcStation", random.nextInt( 1000 ) + 30000 );
@@ -97,6 +254,19 @@ public class DataGeneratorImpl implements DataGenerator {
             jsonArray.put( jsonObj );
         }
         return jsonArray;
+    }
+
+    private String generateRandomString(int length) {
+        final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        StringBuilder sb = new StringBuilder( length );
+        for( int i = 0; i < length; i++ )
+            sb.append( AB.charAt( random.nextInt(AB.length()) ) );
+        return sb.toString();
+    }
+
+    private int getRandomBit() {
+        return random.nextInt( 2 );
     }
 
     private long getGMTTime(Date time) {
