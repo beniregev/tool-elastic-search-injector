563cbac6cc87646b9982a093c24f8d17ff9eeabd
Binyamin Regev <binyamin.regev@nice.com>
2019-12-19 15:27:32 +0200

    Adding default methods to Policy interface to handle arguments generally

--- src/main/java/com/nice/mcr/injector/policies/Policy.java
+++ src/main/java/com/nice/mcr/injector/policies/Policy.java
@@ -1,5 +1,9 @@
 package com.nice.mcr.injector.policies;
 
+import org.springframework.boot.ApplicationArguments;
+
 public interface Policy {
     public void run();
+    default void run(ApplicationArguments args) {}
+    default void setApplicationArguments(ApplicationArguments args) {}
 }
